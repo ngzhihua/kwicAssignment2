de7c38352eec101d4bc80c9c94efcd2ef60adbc9
Zhi Hua <Zhi Hua>
2016-09-02 12:10:41 +0800

    Added test stub for WordsRequired

---
+++ src/test/java/test/WordsRequiredTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WordsRequiredTest {
+
+	@Test
+	public void testGetWordsRequired() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddWordRequired() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFilteredShifts() {
+		fail("Not yet implemented");
+	}
+
+}
